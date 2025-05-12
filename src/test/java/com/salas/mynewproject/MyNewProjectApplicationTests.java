cb82d97e255ea9501278a07b3c178fc66b6cf98b
Vladyslav Hasporian <saslasengland33@gmail.com>
2025-05-12 18:48:48 +0300

    first commit

---
+++ src/test/java/com/salas/mynewproject/MyNewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salas.mynewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNewProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
